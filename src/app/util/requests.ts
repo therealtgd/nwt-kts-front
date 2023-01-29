b72cfeb8de15b8807c1b100e92b673806860e4d8
anaandjelic <ana.andjelic161@gmail.com>
2023-01-29 15:57:27 +0100

    Update requests.ts

--- src/app/util/requests.ts
+++ src/app/util/requests.ts
@@ -8,34 +8,30 @@ type HeaderT = {
 export const apiUrl = 'http://localhost:8080';
 const header: HeaderT = {headers: new HttpHeaders({ 'Content-Type' : 'application/json', responseType : 'json' })}
 
-export function get(httpClient: HttpClient, url: string, contentType: string = 'application/json'){
-  return httpClient.get(apiUrl + url, getHeader(contentType));
+export function get(httpClient: HttpClient, url: string){
+  return httpClient.get(apiUrl + url, header);
 }
 
 export function getWithParams(httpClient: HttpClient, url: string, params: HttpParams){
   return httpClient.get(apiUrl + url, {...header, params});
 }
 
-export function post(httpClient: HttpClient, url: string, requestData : Object, contentType: string = 'application/json'){
-  return httpClient.post(apiUrl + url, requestData, getHeader(contentType));
+export function post(httpClient: HttpClient, url: string, requestData : Object){
+  return httpClient.post(apiUrl + url, requestData, header);
 }
 
 export function postWithoutHeader(httpClient: HttpClient, url: string, requestData : Object){
   return httpClient.post(apiUrl + url, requestData, {});
 }
 
-export function put(httpClient: HttpClient, url: string, requestData : Object, contentType: string = 'application/json'){
-  return httpClient.put(apiUrl + url, requestData, getHeader(contentType));
+export function put(httpClient: HttpClient, url: string, requestData : Object){
+  return httpClient.put(apiUrl + url, requestData, header);
 }
 
-export function patch(httpClient: HttpClient, url: string, requestData : Object, contentType: string = 'application/json'){
-  return httpClient.patch(apiUrl + url, requestData, getHeader(contentType));
+export function patch(httpClient: HttpClient, url: string, requestData : Object){
+  return httpClient.patch(apiUrl + url, requestData, header);
 }
 
-export function del(httpClient: HttpClient, url : string, contentType: string = 'application/json'){
-  return httpClient.delete(apiUrl + url, getHeader(contentType));
-}
-
-function getHeader(contentType: string) {
-  return {headers: new HttpHeaders({ 'Content-Type' : contentType, responseType : 'json' })};
+export function del(httpClient: HttpClient, url : string){
+  return httpClient.delete(apiUrl + url, header);
 }
